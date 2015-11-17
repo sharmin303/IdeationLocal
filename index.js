4cf0179bebf3c69b70a8f400ff3b529f04fcfa52
Sharmin Islam <sharmini@andrew.cmu.edu>
2015-11-16 20:25:26 -0500

    Modified the position of activity log,text area and added logo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
 
 var Canvas = require('canvas')
   , Image = Canvas.Image
@@ -60,8 +63,10 @@ function startNewRound(){
 }
 
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  app.use(express.static(__dirname + '/public'));
 });
 
 
